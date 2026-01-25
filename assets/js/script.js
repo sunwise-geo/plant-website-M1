8a386c27fc653d0a919229353fd6867e00a69174
sunwise-geo <thilenigp@gmail.com>
2026-01-25 21:00:51 +0100

    finition

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 
 const menuHamburger = document.querySelector(".menu_hamburger")
 const navLinks = document.querySelector(".nav-links")
+const body = document.querySelector("body")
 
 // pour que le menu hamburger fonctionne sur mobile à chaque clique sur le bouton la classe mobile-menu apparaît et quand je reclique la classe disparaît
 menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile_menu')
+    body.classList.toggle('mobile-menu-visible')
 })
 
 
